678161c65bd20087df4b8cd22f6e0e80ff65c4ed
spacj <100452860+spacj@users.noreply.github.com>
2024-05-26 16:38:02 +0200

    Update Hero.js

--- src/pages/components/Hero.js
+++ src/pages/components/Hero.js
@@ -12,13 +12,27 @@ function Hero({item}) {
         Learn More
       </Link>
       </div>
-      <div className="flex flex-col justify-items-center w-[100vw] -ml-[3vw] mt-[3vh] bg-[#fe573d] pb-[3vh]">
+      <div className="flex flex-col justify-items-center w-[100vw] -ml-[3vw] mt-[3vh] bg-[#fe573d] pb-[4vh]">
        <img src="https://www.goodnet.org/photos/620x0/35438_hd.jpg" alt="" className="" />
        <h2 className="text-white pl-[7vw] pt-[2vh] font-bold text-3xl px-4">{item.featureone}</h2>
        <h2 className="text-white pl-[7vw] pt-[0vh] font-light text-lg px-4">{item.featureonedesc}</h2>
        <Link to="/about" className="btn-hero-or min-w-[50%] self-start ml-[7vw] mt-[2vh]" style={{ textDecoration: 'none' }}>
          Discover Tour
        </Link>
+      </div>
+      <div className="flex flex-col justify-items-center w-[100vw] -ml-[3vw] bg-white pb-[3vh]">
+       <img src="https://static.ohga.it/wp-content/uploads/sites/24/2024/05/iStock-2060958853.jpg" alt="" className="" />
+       <h2 className="text-black pl-[7vw] text-right pt-[2vh] font-bold text-3xl px-4">{item.featuretwo}<span className="text-[#fe573d]">PRO</span></h2>
+       <h2 className="text-[#3f636bfe] pl-[7vw] text-right pt-[0vh] font-light text-lg px-4">{item.featuretwodesc}</h2>
+       <Link to="/about" className="btn-hero text-center min-w-[50%] self-end mr-[7vw] mt-[2vh]" style={{ textDecoration: 'none' }}>
+         Urban Guides
+       </Link>
+      </div>
+      <div className="flex flex-col justify-items-center w-[100vw] -ml-[3vw] bg-[#fe573d] pb-[3vh]">
+      </div>
+      <div className="flex flex-col justify-items-center w-[100vw] -ml-[3vw] bg-white pb-[3vh]">
+      </div>
+      <div className="flex flex-col justify-items-center w-[100vw] -ml-[3vw] bg-[#fe573d] pb-[3vh]">
       </div>
     </>
 
