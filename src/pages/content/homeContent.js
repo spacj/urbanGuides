0e0acd7d53697779163b113e83628dca4da3501a
spacj <100452860+spacj@users.noreply.github.com>
2024-05-26 16:23:27 +0200

    Update homeContent.js

--- src/pages/content/homeContent.js
+++ src/pages/content/homeContent.js
@@ -1,10 +1,14 @@
 const HomeContent = {
+  realtitle: 'Urban Guides',
+  realdesc: 'At LiftCrafter, we redefine heavy equipment shopping. Our user-friendly e-commerce platform offers effortless navigation, expert guidance, and competitive prices. We are your trusted source for top-quality machinery, providing ultimate convenience. Experience the future of heavy equipment shopping, explore, and purchase with confidence',
   title:'Welcome',
   subtitle:'to the',
   mainImg:'images/mapImage.png',
   about: 'Proud to be one of the largest communities of travellers and locals worldwide.',
   featureone: 'Tours and Activities in Milano',
-  featureonedesc: 'Our website is born with the goal to help you find only valuable and worthy tours and activities, while excluding all the tourist traps that nowaday are everywhere'
+  featureonedesc: 'Our website is born with the goal to help you find only valuable and worthy tours and activities, while excluding all the tourist traps that nowaday are everywhere',
+  featuretwo: 'Travel like a ',
+  featuretwodesc: 'Discover our free self-guided tours, tips, tricks, recommendations and reviews to travel and enjoy your trip in the best way ever! We strongly believe that anyone deserves to travel and learn in a cheap way, lets help us make culture accessible to everybody',
 }
 
 export default HomeContent;
