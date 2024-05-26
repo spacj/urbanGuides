bd02b7b832fd85a30e5a9010aeec1de1cee35bfb
spacj <100452860+spacj@users.noreply.github.com>
2024-05-26 16:38:58 +0200

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Hero from './components/Hero.js';
 import HomeContent from './content/homeContent.js';
+import Footer from './components/Footer.js';
 
 const Home = (item) => {
 
@@ -8,7 +9,7 @@ const Home = (item) => {
     <div className="container-fluid">
       <div className="h-[13vh]"></div>
       <div className="-mt-[5vh]"><Hero item={HomeContent}/></div>
-      
+      <div><Footer item={HomeContent} /></div>
     </div>
   );
 };
